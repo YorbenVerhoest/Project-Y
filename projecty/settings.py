b31f0f3e16be16d560bbd0006419ab4ab4efde7e
YorbenV <yorben.verhoest@ondernemersnetwerk.be>
2023-10-31 10:57:28 +0100

    update

--- projecty/settings.py
+++ projecty/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # The absolute filesystem path to the directory that will hold your static files.
-STATIC_ROOT = '/home/YorbenV/yorbenv.pythonanywhere.com/static/'
+STATIC_ROOT = '/home/YorbenV/yorbenv.pythonanywhere.com/static_root/'
 
 # Additional locations of static files
 STATICFILES_DIRS = [
