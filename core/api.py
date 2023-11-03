ef67a212e79ad51746c19691fea6574d8a22d550
YorbenV <yorben.verhoest@ondernemersnetwerk.be>
2023-11-03 09:31:19 +0100

    test

--- core/api.py
+++ core/api.py
@@ -5,13 +5,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import viewsets
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
 from rest_framework_jwt.views import RefreshJSONWebToken
 
 from core import serializers
 from core import models as core
-from rest_framework_simplejwt.authentication import JWTAuthentication
 
 class UserLoginView(APIView):
     def post(self, request):
